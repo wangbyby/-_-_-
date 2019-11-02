5aefd98d3d46036aedd8a67d0d7e6f2d22ad5b6f
wangbyby <42870190+wangbyby@users.noreply.github.com>
2019-11-02 21:15:55 +0800

    sendemails

--- server.py
+++ server.py
@@ -1,29 +1,53 @@
 from flask import Flask, render_template, send_file,  json, jsonify, make_response, request
 import json
+import  yagmail
 import location
 app = Flask(__name__, static_url_path='')
 
+#send emails 的配置
+email_args = {
+    "user":"3453667697@qq.com",
+    "password":"euymebcvccqcdaei",
+    "host":"smtp.qq.com",
+    "port":"465"
+}
 
 @app.route('/')
-def hello_flask():
+def hello_flask(): #根目录
     return render_template('datastruct_inuse.htm')
 
 
 @app.route('/query', methods=['GET', 'POST'])
-def query_json():
+def query_json():#返回经纬度
     with open('location.json') as f:
         json_file = json.load(f)
         return jsonify(json_file)
 
 
 @app.route('/get_file/<file_name>', methods=['GET', 'POST'])
-def get_file(file_name):
+def get_file(file_name): #返回服务器资源
     response = make_response(send_file(file_name))
     response.headers["Content-Disposition"] = "attachment; filename={};".format(file_name)
     return response
 
+@app.route('/email',methods=['GET','POST'])
+def send_email(): #发送邮件
+    if request.method == 'GET':
+        return render_template('tips.htm')
+    elif request.method == 'POST':
+        data = None
+        for i in request.form:
+            data = json.loads(i)
+            print(data)
+        receive = data['emails']
+        email = yagmail.SMTP(**email_args)
+        subject = data['subject']
+        contents = data['contents']
+        email.send(to=receive, subject=subject,contents=contents)
+
+        return "OK"
 @app.route('/data', methods=['GET',"POST"])
-def data():
+def data(): #返回要查询的路径
     res = None
     for i in request.form:
         search = json.loads(i)
