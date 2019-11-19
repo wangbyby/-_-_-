50ffc5a07b734740824a4ea1432c11cad5930583
wangbyby <42870190+wangbyby@users.noreply.github.com>
2019-11-19 09:31:08 +0800

    sql insert into

--- db.sql
+++ db.sql
@@ -1,6 +1,8 @@
 #pgsql
 create database steam
 \c steam
+#游戏发行商
+#一般来说 发行商与开发商 不一样
 create table gc(
     gcname character varying(20) primary key,
     address character varying(40) NOT NUll default '',
@@ -8,6 +10,7 @@ create table gc(
 )WITH (
     OIDS = FALSE
 )TABLESPACE pg_default;
+
 
 #游戏玩家
 create table player( 
@@ -20,7 +23,7 @@ create table player(
     OIDS = FALSE
 )TABLESPACE pg_default;
 
-
+#游戏
 create table game(
     gname character varying(20) primary key,
     price money NOT NUll default 100
@@ -28,7 +31,7 @@ create table game(
     OIDS = FALSE
 )TABLESPACE pg_default;
 
-
+#社区
 create table community(
     cid bigserial primary key,
     content character varying(120) NOT NUll default '',
@@ -38,6 +41,7 @@ create table community(
     OIDS = FALSE
 )TABLESPACE pg_default;
 
+#游戏发行商 发行 游戏
 create table issue(
     gcname character varying(20) references gc(gcname) on update CASCADE on delete CASCADE   ,
     gname  character varying(20) REFERENCES game(gname) on update CASCADE on delete CASCADE ,
@@ -49,20 +53,24 @@ create table issue(
     OIDS = FALSE
 )TABLESPACE pg_default;
 
+#玩家 购买 游戏
 #lpt : last play time 最近运行时间
 #bt : 购买时间
+#btype : 购买方式 : 微信, 支付宝, visa, 比特币...
+#sid : 序列号, 自动增长
 create table own(
-    uname character varying(15) references player(uname) on update CASCADE on delete CASCADE   ,
+    uname character varying(15) references player(uname) on update CASCADE on delete CASCADE,
     gname character varying(20) references game(gname) on update CASCADE on delete CASCADE ,
     lrt timestamp  with time zone NOT NUll,  
     bt timestamp  with time zone NOT NUll,
-    btype smallint NOT NUll default 0,
+    btype  character varying(10) NOT NUll,
     sid bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
     primary key(uname,gname)
 )WITH (
     OIDS = FALSE
 )TABLESPACE pg_default;
 
+#游戏发布社区内容
 create table com_con(
     cid bigserial references community(cid) on update CASCADE on delete CASCADE,
     gname character varying(20) references game(gname) on update CASCADE on delete CASCADE ,
@@ -72,3 +80,10 @@ create table com_con(
 )TABLESPACE pg_default;
 
 
+#'' : 字符串
+insert into steam.gc(gcname,address,info) values ('NewWorld', 'American','小公司');
+insert into steam.gc(gcname,address,info) values ('Valve', 'American','黑心商人');
+insert into steam.gc(gcname,address,info) values ('Paradox', 'Sweden','沙盒类游戏');
+insert into steam.gc(gcname,address,info) values ('Crytek', 'Germany frankfurt','深受盗版困扰');
+insert into steam.gc(gcname,address,info) values ('Petroglyph', 'American Las Vegas','前西木员工')
+insert into steam.gc(gcname,address,info) values ('505 Games', 'Italy','没啥')
